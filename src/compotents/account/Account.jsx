40dbfed01b16feab0a98fed590f943dcd215690e
GATE31 <gate31@MacBook-Pro-web-razrabotcik.local>
2021-08-03 22:47:24 +0300

    route

---
+++ src/compotents/account/Account.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+function Home() {
+    return (
+        <div>Главная</div>
+    );
+}
+
+export default Home;
---
+++ src/compotents/type/Type.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+function Account() {
+    return (
+        <div>Аккаунт</div>
+    );
+}
+
+export default Account;
